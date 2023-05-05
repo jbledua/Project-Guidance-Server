b2191e84061b4ae20604a5601cc59f652d3b1c27
Josiah Ledua <jledua@lakeheadu.ca>
2023-05-05 15:08:33 -0400

    Buit Test Response System

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const { initializeApp } = require('firebase/app');
-const { getFirestore, collection, getDocs, query, where, doc, orderBy, onSnapshot } = require('firebase/firestore');
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, query, where, orderBy, onSnapshot } from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBGPf3b7Cafiq7Yla4FoCLDiScjVAKTcQ4",
@@ -34,7 +34,8 @@ async function getThreadsForUser(db, uid) {
   // Get a list of messages for a specific thread
   async function getMessagesForThread(db, threadId) {
     const messagesCol = collection(db, 'messages');
-    const q = query(messagesCol, where('threadId', '==', threadId), orderBy('timestamp'));
+    //const q = query(messagesCol, where('threadId', '==', threadId), orderBy('timestamp'));
+    const q = query(messagesCol, where('threadId', '==', threadId));
     const messageSnapshot = await getDocs(q);
     const messageList = messageSnapshot.docs.map(doc => doc.data());
     return messageList;
@@ -53,7 +54,6 @@ async function getThreadsForUser(db, uid) {
       });
     });
   }
-  
 
 // Test the getThreads function
 // (async () => {
@@ -95,26 +95,324 @@ async function getThreadsForUser(db, uid) {
   // })();
 
   // Test the getMessagesForThread function
+  // (async () => {
+  //   try {
+  //     const threadId = 'dORB3L9x9BxCsPtBxf1i'; // Everyone Thead
+  //     const messages = await getMessagesForThread(db, threadId);
+
+  //     console.log("Messages for thread: " + threadId);
+      
+  //     // Print out the messages
+  //     for (let index = 0; index < messages.length; index++) {
+  //       const message = messages[index];
+  //       console.log(message.senderName,": ", message.content);
+  //     }
+
+  //   } catch (error) {
+  //     console.error('Error getting messages:', error);
+  //   }
+  // })();
+
+  
+import { Configuration, OpenAIApi } from "openai";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const configuration = new Configuration({
+    organization: "org-QGPFrC2VuY4POelQsOdLm5gq",
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+  //async function testOpenAI() {
+  // (async () => {
+  //   try {
+  //     const completion = await openai.createChatCompletion({
+  //       model: "gpt-3.5-turbo",
+  //       messages: [{role: "user", content: "Hello world"}],
+  //     });
+  //     console.log(completion.data.choices[0].message);
+      
+  //   } catch (error) {
+  //     console.error('Error listing models:', error);
+  //   }
+  // })();
+
+  // Test the getMessagesForThread function
+  // (async () => {
+  //   try {
+  //     const threadId = 'dORB3L9x9BxCsPtBxf1i';
+  //     const messages = await getMessagesForThread(db, threadId);
+  
+  //     console.log("Messages for thread: " + threadId);
+  
+  //     for (let index = 0; index < messages.length; index++) {
+  //       const message = messages[index];
+  //       console.log(message.senderName, ": ", message.content);
+  //     }
+  
+  //   } catch (error) {
+  //     console.error('Error getting messages:', error);
+  //   }
+  // })();
+  
+  // const threadId = 'dORB3L9x9BxCsPtBxf1i';
+  
+  // const unsubscribe = listenForNewMessages(db, threadId, (message) => {
+  //   console.log(message.senderName, ': ', message.content);
+  // });
+
+  
+
+  // Test the getMessagesForThread function
+  // (async () => {
+  //   try {
+  //     const threadId = 'dORB3L9x9BxCsPtBxf1i';
+  //     const messages = await getMessagesForThread(db, threadId);
+  
+  //     console.log("Messages for thread: " + threadId);
+  
+  //     for (let index = 0; index < messages.length; index++) {
+  //       const message = messages[index];
+  //       console.log(message.senderName, ": ", message.content);
+  //     }
+  
+  //   } catch (error) {
+  //     console.error('Error getting messages:', error);
+  //   }
+  // })();
+
+
+  // function listenForNewMessagesAfterNow(db, threadId, onMessageReceived) {
+  //   const messagesCol = collection(db, 'messages');
+  //   const currentTimestamp = new Date();
+  //   const q = query(
+  //     messagesCol,
+  //     where('threadId', '==', threadId),
+  //     where('timestamp', '>', currentTimestamp),
+  //     orderBy('timestamp')
+  //   );
+  
+  //   return onSnapshot(q, (snapshot) => {
+  //     snapshot.docChanges().forEach((change) => {
+  //       if (change.type === 'added') {
+  //         const message = change.doc.data();
+  //         onMessageReceived(message);
+  //       }
+  //     });
+  //   });
+  // }
+  
+  // const threadId = 'dORB3L9x9BxCsPtBxf1i'; // Replace with your thread ID
+  // const unsubscribe = listenForNewMessagesAfterNow(db, threadId, (message) => {
+
+
+  //   console.log(message.senderName, ': ', message.content);
+  // });
+
+
+
+  const systemUIDs = ["feHUM4T2IVZri1oLriUknGl7PJ22"];
+  const assistantUIDs = ["6HA4v0qlNEQBIliZLJK5RLDa28z1"];
+
+  function formatMessages(messages) {
+    // Sort messages by timestamp in descending order
+    messages.sort((a, b) => b.timestamp - a.timestamp);
+  
+    // Take the 10 most recent messages
+    const recentMessages = messages.slice(0, 10);
+  
+    return recentMessages.map(message => {
+      let role;
+  
+      if (systemUIDs.includes(message.senderId)) {
+        role = "system";
+      } else if (assistantUIDs.includes(message.senderId)) {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+  
+      return { role: role, content: message.content };
+    });
+  }
+
+  
+  // Test the getMessagesForThread function
+  // (async () => {
+  //   try {
+  //     const threadId = 'dORB3L9x9BxCsPtBxf1i';
+  //     const messages = await getMessagesForThread(db, threadId);
+  
+  //     console.log("Messages for thread: " + threadId);
+
+  //     const fomatedMessages = formatMessages(messages);
+  
+  //     for (let index = 0; index < fomatedMessages.length; index++) {
+  //       const message = fomatedMessages[index];
+  //       console.log(message.role, ": ", message.content);
+  //     }
+  
+  //   } catch (error) {
+  //     console.error('Error getting messages:', error);
+  //   }
+  // })();
+  
+  // const threadId = 'dORB3L9x9BxCsPtBxf1i';
+  // const allMessages = [];
+  // const unsubscribe = listenForNewMessages(db, threadId, (message) => {
+
+  //   console.log(message.senderName, ': ', message.content);
+  // });
+
+
+  
+
+
+  // async function generateChatResponse(messages) {
+  //   try {
+  //     const formattedMessages = messages.map(message => {
+
+  //       const role = "";// = message.senderId === "system" ? "system" : "user";
+
+  //       switch (message.senderId) {
+  //         case "system":
+  //           role = "system";
+  //           break;
+  //         case "assistant":
+  //           role = "assistant";
+  //           break;
+  //         default:
+  //           role = "user";
+  //           break;
+  //       }
+  //       return { role: role, content: message.content };
+  //     });
+  
+  //     const completion = await openai.createChatCompletion({
+  //       model: "gpt-3.5-turbo",
+  //       messages: formattedMessages,
+  //     });
+  
+  //     console.log(completion.data.choices[0].message);
+  //   } catch (error) {
+  //     console.error('Error generating chat response:', error);
+  //   }
+  // }
+
+  // const messages = [
+  //   { senderId: "user1", content: "Hello world" },
+  //   { senderId: "system", content: "System message" },
+  //   { senderId: "user2", content: "Another user message" },
+  // ];
+  
+  // generateChatResponse(messages);
+
+  async function generateChatResponse(messages) {
+    try {
+      // const formattedMessages = messages.map(message => {
+      //   const role = message.senderId === "system" ? "system" : "user";
+      //   return { role: role, content: message.content };
+      // });
+
+      //const formattedMessages = formatMessages(messages);
+
+      // for (let index = 0; index < messages.length; index++) {
+      //   const element = messages[index];
+      //   console.log(element.role, ": ", element.content);
+        
+      // }
+  
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+      });
+  
+      console.log("assistant : ", completion.data.choices[0].message.content);
+
+      return completion.data.choices[0].message.content;
+    } catch (error) {
+      console.error('Error generating chat response:', error);
+    }
+  }
+  
+
+  const messages = [
+    { senderId: "feHUM4T2IVZri1oLriUknGl7PJ22", content: "You will be providing computer assistance for users" },
+    { senderId: "feHUM4T2IVZri1oLriUknGl7PJ22", content: "Provide simple instuctions to resolve the issuse the user provides" },
+    { senderId: "u00vbNFoshfcmt3rJFbBWSNHIyk2", content: "Hello my name is Janelle" },
+    { senderId: "6HA4v0qlNEQBIliZLJK5RLDa28z1", content: "Hello Janelle, how can I help you today?" },
+    { senderId: "u00vbNFoshfcmt3rJFbBWSNHIyk2", content: "How I change what program opens when I click on a pdf?" },
+  ];
+  
+
+  import { addDoc } from 'firebase/firestore';
+
+  async function writeMessageToThread(db, message) {
+    try {
+      const messagesCol = collection(db, 'messages');
+      const newMessage = {
+        content: message.content,
+        read: message.read,
+        senderName: message.senderName,
+        senderId: message.senderId,
+        threadId: message.threadId,
+        timestamp: message.timestamp,
+      };
+      await addDoc(messagesCol, newMessage);
+      console.log('Message written successfully');
+    } catch (error) {
+      console.error('Error writing message:', error);
+    }
+  }
+
+
   (async () => {
     try {
-      const threadId = 'dORB3L9x9BxCsPtBxf1i'; // Everyone Thead
+      const threadId = 'nZolESlGu8QU2quRu1Ps';
+
+      // Get messages for thread from Firestore
       const messages = await getMessagesForThread(db, threadId);
 
-      console.log("Messages for thread: " + threadId);
-      
-      // Print out the messages
-      for (let index = 0; index < messages.length; index++) {
-        const message = messages[index];
-        console.log(message.senderName,": ", message.content);
-      }
+      // Format messages for OpenAI
+      const formattedMessages = formatMessages(messages);
 
+      // Generate chat response from OpenAI
+      const response = await generateChatResponse(formattedMessages);
+
+
+      const responseMessage = {
+        content: response,
+        read: ['6HA4v0qlNEQBIliZLJK5RLDa28z1'],
+        senderName: 'AI Guideance',
+        senderId: '6HA4v0qlNEQBIliZLJK5RLDa28z1',
+        threadId: threadId,
+        timestamp: new Date(),
+      };
+
+      // Write message to Firestore
+      await writeMessageToThread(db, responseMessage);
+  
+      // Print messages to console
+      console.log("Messages for thread: " + threadId);
+  
+      // 
+      for (let index = 0; index < formattedMessages.length; index++) {
+        const message = formattedMessages[index];
+        console.log(message.role, ": ", message.content);
+      }
+  
     } catch (error) {
       console.error('Error getting messages:', error);
     }
   })();
+  
 
-  const threadId = 'dORB3L9x9BxCsPtBxf1i'; // Replace with the thread ID you want to listen for new messages
 
-  const unsubscribe = listenForNewMessages(db, threadId, (message) => {
-    console.log(message.senderName, ': ', message.content);
-  });
+
+  
+  
+  
+  
+  
